cb06d506dbac1829ab2936cc562bb3dd6dac59bb
unknown <eunice_galvez_10@hotmail.com>
2020-07-09 21:35:18 -0500

    router link added for heroes details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { HeroesComponent } from './heroes/heroes.component';
+/*
+This part configure you configure your
+routes. Routes tell the Router which view to display when a
+ user clicks a link or pastes a URL into the browser address bar.
+*/
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  // method supplies the service providers and
+  // directives needed for routing
   exports: [RouterModule]
+  // AppRoutingModule exports RouterModule so
+  // it will be available throughout the app.
 })
 export class AppRoutingModule { }
